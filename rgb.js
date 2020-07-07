766762c714c129d171b8a39b0cab5b2a0ccba857
abentley001 <abentley@elevationchurch.org>
2020-07-06 20:56:49 -0400

    First go at remotely enabling vegas mode

--- rgb.js
+++ rgb.js
@@ -15,20 +15,7 @@ gpiRed.digitalWrite(0); // Turn RED LED off
 gpiGreen.digitalWrite(0); // Turn GREEN LED off
 gpiBlue.digitalWrite(0); // Turn BLUE LED off
 
-setInterval(() => {
-  i++;
-  ledColor.spin(i);
-	gpiRed.pwmWrite(Math.round(ledColor._r)); // Turn RED LED off
-	gpiGreen.pwmWrite(Math.round(ledColor._g)); // Turn GREEN LED off
-	gpiBlue.pwmWrite(Math.round(ledColor._b));
-	console.log(ledColor._r); // Turn RED LED off
-	console.log(ledColor._g); // Turn GREEN LED off
-	console.log(ledColor._b);
-  ledColor.spin(-i);
-  if (i > 359){
-    i = 0;
-  }
-}, 10)
+
 
 
 http.listen(8080); //listen to port 8080
@@ -38,30 +25,47 @@ function handler (req, res) { //what to do on requests to port 8080
     //console.log('handler', req.body);
     req.on('data', function (data) {
         console.log('request data', data.toString())
-	var color = JSON.parse(data.toString());
+				var color = JSON.parse(data.toString());
         gpiRed.pwmWrite((parseInt(color.red))); //set RED LED to specified value
         gpiGreen.pwmWrite((parseInt(color.green))); //set GREEN LED to specified value
         gpiBlue.pwmWrite((parseInt(color.blue))); //set BLUE LED to specified value
-    })
+
+				clearInterval(vegasMode);
+
+				if (data.mode == 'vegas'){
+					let vegasMode = setInterval(() => {
+					  i++;
+					  ledColor.spin(i);
+						gpiRed.pwmWrite(Math.round(ledColor._r)); // Turn RED LED off
+						gpiGreen.pwmWrite(Math.round(ledColor._g)); // Turn GREEN LED off
+						gpiBlue.pwmWrite(Math.round(ledColor._b));
+					  ledColor.spin(-i);
+					  if (i > 359){
+					    i = 0;
+					  }
+					}, 10)
+				}
+
+		})
     return res.end();
   }
 
-io.sockets.on('connection', function (socket) {// Web Socket Connection
-  socket.on('rgbLed', function(data) { //get light switch status from client
-    console.log('socket log ', data); //output data from WebSocket connection to console
-
-    //for common anode RGB LED  255 is fully off, and 0 is fully on, so we have to change the value from the client
-    redRGB=parseInt(data.red);
-    greenRGB=parseInt(data.green);
-    blueRGB=parseInt(data.blue);
-
-    console.log("rbg: " + redRGB + ", " + greenRGB + ", " + blueRGB); //output converted to console
-
-    gpiRed.pwmWrite(redRGB); //set RED LED to specified value
-    gpiGreen.pwmWrite(greenRGB); //set GREEN LED to specified value
-    gpiBlue.pwmWrite(blueRGB); //set BLUE LED to specified value
-  });
-});
+// io.sockets.on('connection', function (socket) {// Web Socket Connection
+//   socket.on('rgbLed', function(data) { //get light switch status from client
+//     console.log('socket log ', data); //output data from WebSocket connection to console
+//
+//     //for common anode RGB LED  255 is fully off, and 0 is fully on, so we have to change the value from the client
+//     redRGB=parseInt(data.red);
+//     greenRGB=parseInt(data.green);
+//     blueRGB=parseInt(data.blue);
+//
+//     console.log("rbg: " + redRGB + ", " + greenRGB + ", " + blueRGB); //output converted to console
+//
+//     gpiRed.pwmWrite(redRGB); //set RED LED to specified value
+//     gpiGreen.pwmWrite(greenRGB); //set GREEN LED to specified value
+//     gpiBlue.pwmWrite(blueRGB); //set BLUE LED to specified value
+//   });
+// });
 
 process.on('SIGINT', function () { //on ctrl+c
   gpiRed.digitalWrite(0); // Turn RED LED off
